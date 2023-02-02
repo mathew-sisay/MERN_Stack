ccb472bb17442ee553536d5d8335a2e42f376104
mathew-sisay <mathewsisay835@gmail.com>
2023-02-02 14:42:31 +0000

    error middleware setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,37 @@
+const asyncHandler = require('express-async-handler')
 // @desc  Get goals
 // @route Get /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals'})
-}
+})
 
 // @desc  set goals
 // @route Post /api/goals
 // @access Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error ('please add a text field ')
+    } 
+
     res.status(200).json({ message: 'set goals'})
-}
+})
 
 
 // @desc  Update goals
 // @route Put /api/goals/:id
 // @access Private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goals ${req.params.id}`})
-}
+})
 
 // @desc  Delete goals
 // @route Delete /api/goals/:id
 // @access Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete goals ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
